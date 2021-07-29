d7ddb66a448b99b1d1f2288acf4c5fa6b7196da7
adil8539 <52648711+adil8539@users.noreply.github.com>
2021-07-29 17:41:23 +0530

    Initialize

---
+++ inc/config/shop_inventory.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 30, 2020 at 01:54 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shop_inventory`
+--
+
+-- --------------------------------------------------------
+
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `productID` int(11) NOT NULL,
+  `itemNumber` varchar(255) NOT NULL,
+  `itemName` varchar(255) NOT NULL,
+  `discount` float NOT NULL DEFAULT '0',
+  `stock` int(11) NOT NULL DEFAULT '0',
+  `unitPrice` float NOT NULL DEFAULT '0',
+  `imageURL` varchar(255) NOT NULL DEFAULT 'imageNotAvailable.jpg',
+  `status` varchar(255) NOT NULL DEFAULT 'Active',
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userID` int(11) NOT NULL,
+  `fullName` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userID`, `fullName`, `username`, `password`, `status`) VALUES
+(5, 'Guest', 'guest', '81dc9bdb52d04dc20036dbd8313ed055', 'Active'),
+(6, 'a', 'a', '0cc175b9c0f1b6a831c399e269772661', 'Active'),
+(7, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Active');
+
+-- --------------------------------------------------------
+
+
+--
+-- Indexes for dumped tables
+--
+
+
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`productID`);
+
+
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`);
+
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `vendor`
+--
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ model/item/itemDetailsSearchTableCreator.php
@@ -0,0 +1,43 @@
+<?php
+	require_once('../../inc/config/constants.php');
+	require_once('../../inc/config/db.php');
+	
+	$itemDetailsSearchSql = 'SELECT * FROM item';
+	$itemDetailsSearchStatement = $conn->prepare($itemDetailsSearchSql);
+	$itemDetailsSearchStatement->execute();
+	
+	$output = '<table id="itemDetailsTable" class="table table-sm table-striped table-bordered table-hover" style="width:100%">
+				<thead>
+					<tr>
+						<th>Book ID</th>
+						<th>Book Number</th>
+						<th>Book Name</th>
+						<th>Author</th>
+						<th>Stock</th>
+						<th>Price (₹)</th>
+						<th>Status</th>
+						
+					</tr>
+				</thead>
+				<tbody>';
+	
+	// Create table rows from the selected data
+	while($row = $itemDetailsSearchStatement->fetch(PDO::FETCH_ASSOC)){
+		
+		$output .= '<tr>' .
+						'<td>' . $row['productID'] . '</td>' .
+						'<td>' . $row['itemNumber'] . '</td>' .
+						'<td><a href="#" class="itemDetailsHover" data-toggle="popover" id="' . $row['productID'] . '">' . $row['itemName'] . '</a></td>' .
+						'<td>' . $row['description'] . '</td>' .
+						'<td>' . $row['stock'] . '</td>' .
+						'<td>' . $row['unitPrice'] . '</td>' .
+						'<td>' . $row['status'] . '</td>' .
+						
+					'</tr>';
+	}
+	
+	$itemDetailsSearchStatement->closeCursor();
+	
+
+	echo $output;
+?>
